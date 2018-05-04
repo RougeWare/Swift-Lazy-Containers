3120ff447bcae431c4b7ecddb0dd2eea2de46731
Ben Leggiero <BenLeggiero@Gmail.com>
2018-05-04 15:38:55 -0400

    Workaround for SR-7604

--- Lazy.swift
+++ Lazy.swift
@@ -3,7 +3,7 @@
 //  Lazy in Swift 4
 //
 //  Created by Ben Leggiero on 2018-05-04.
-//  Version 1.0.0 (last edited 2018-05-04)
+//  Version 1.1.0 (last edited 2018-05-04)
 //  Copyright CC0 © 2018 - https://creativecommons.org/publicdomain/zero/1.0/
 //
 
@@ -11,6 +11,7 @@ import Foundation
 
 
 
+/// Simply initializes a value
 public typealias Initializer<Value> = () -> Value
 
 
@@ -18,8 +19,10 @@ public typealias Initializer<Value> = () -> Value
 /// Defines how a lazy pattern should look
 public protocol LazyPattern {
     
+    /// The type of the value that will be lazily-initialized
     associatedtype Value
     
+    /// Gets the value, possibly initializing it first
     var value: Value { mutating get }
 }
 
@@ -31,8 +34,9 @@ public struct Lazy<_Value>: LazyPattern {
     public typealias Value = _Value
     
     
+    
     /// Holds the internal value of this `Lazy`
-    private var valueHolder: ValueHolder
+    private var valueHolder: ValueHolder<Value>
     
     
     /// Creates a non-resettable lazy with the given value initializer. That closure will be called the very first time
@@ -64,17 +68,19 @@ public struct Lazy<_Value>: LazyPattern {
             }
         }
     }
+}
+
+
+
+// NOTE: This would be nested within `Lazy`, but that caused a runtime crash documented in
+// https://bugs.swift.org/browse/SR-7604
+/// Takes care of keeping track of the state, value, and initializer as needed
+private enum ValueHolder<Value> {
+    /// Indicates that a value has been cached, and contains that cached value
+    case hasValue(Value)
     
-    
-    
-    /// Takes care of keeping track of the state, value, and initializer as needed
-    private enum ValueHolder {
-        /// Indicates that a value has been cached, and contains that cached value
-        case hasValue(Value)
-        
-        /// Indicates that the value has not yet been created, and contains its initializer
-        case unset(Initializer<Value>)
-    }
+    /// Indicates that the value has not yet been created, and contains its initializer
+    case unset(Initializer<Value>)
 }
 
 
@@ -86,11 +92,12 @@ public struct Lazy<_Value>: LazyPattern {
 ///              instead wherever possible.
 public struct ResettableLazy<_Value>: LazyPattern {
     
-    public typealias Value = _Value!
+    public typealias Value = _Value
+    
     
     
     /// Holds the internal value of this `Lazy`
-    private var valueHolder: ValueHolder
+    private var valueHolder: ResettableValueHolder<Value>
     
     
     /// Creates a resettable lazy pattern with the given value initializer. That closure will be called every time a
@@ -98,7 +105,7 @@ public struct ResettableLazy<_Value>: LazyPattern {
     ///
     ///   1. The first time `value` is called, the result from `initializer` will be cached and returned
     ///   2. Subsequent calls to get `value` will return the cached value
-    ///   3. If `clear()` or `value = nil` is called, the state is set back to step 1
+    ///   3. If `clear()` is called, the state is set back to step 1
     ///
     /// - Parameter initializer: The closure that will be called every time a value is needed
     public init(initializer: @escaping Initializer<Value>) {
@@ -109,11 +116,11 @@ public struct ResettableLazy<_Value>: LazyPattern {
     /// Sets or returns the value held within this struct.
     ///
     /// If there is none, it is created using the initializer given when this struct was initialized. This process only
-    /// happens on the first call to `value`; subsequent calls are guaranteed to return the cached value from the first
-    /// call.
+    /// happens on the first call to `value`;
+    /// subsequent calls are guaranteed to return the cached value from the first call.
     ///
-    /// You may also use this to set the value manually if you wish. Setting this to `nil` is equivalent to calling
-    /// the `clear()` function.
+    /// You may also use this to set the value manually if you wish.
+    /// That value will stay cached until `clear()` is called.
     public var value: Value {
         mutating get {
             switch valueHolder {
@@ -128,12 +135,7 @@ public struct ResettableLazy<_Value>: LazyPattern {
         }
         
         set {
-            if let newValue = newValue {
-                valueHolder = .hasValue(newValue, valueHolder.initializer)
-            }
-            else {
-                clear()
-            }
+            valueHolder = .hasValue(newValue, valueHolder.initializer)
         }
     }
     
@@ -143,25 +145,27 @@ public struct ResettableLazy<_Value>: LazyPattern {
     public mutating func clear() {
         valueHolder = .unset(valueHolder.initializer)
     }
+}
+
+
+
+// NOTE: This would be nested within `ResettableLazy`, but that caused a runtime crash documented in
+// https://bugs.swift.org/browse/SR-7604
+/// Takes care of keeping track of the state, value, and initializer as needed
+private enum ResettableValueHolder<Value> {
+    /// Indicates that a value has been cached, and contains that cached value, and the initializer in case the
+    /// value is cleared again later on
+    case hasValue(Value, Initializer<Value>)
     
+    /// Indicates that the value has not yet been created, and contains its initializer
+    case unset(Initializer<Value>)
     
-    
-    /// Takes care of keeping track of the state, value, and initializer as needed
-    private enum ValueHolder {
-        /// Indicates that a value has been cached, and contains that cached value, and the initializer in case the
-        /// value is cleared again later on
-        case hasValue(Value, Initializer<Value>)
-        
-        /// Indicates that the value has not yet been created, and contains its initializer
-        case unset(Initializer<Value>)
-        
-        /// Finds and returns the initializer held within this enum case
-        var initializer: Initializer<Value> {
-            switch self {
-            case .hasValue(_, let initializer),
-                 .unset(let initializer):
-                return initializer
-            }
+    /// Finds and returns the initializer held within this enum case
+    var initializer: Initializer<Value> {
+        switch self {
+        case .hasValue(_, let initializer),
+             .unset(let initializer):
+            return initializer
         }
     }
 }
@@ -182,7 +186,7 @@ public class FunctionalLazy<_Value>: LazyPattern {
     
     
     /// The closure called every time a value is needed
-    private var x: Initializer<Value>! = nil
+    private var valueHolder: Initializer<Value>! = nil
     
     /// Guarantees that, on first-init, only one thread initializes the value. After that, this is set to `nil` because
     /// subsequent threads can safely access the value without setting it again.
@@ -197,12 +201,12 @@ public class FunctionalLazy<_Value>: LazyPattern {
     ///
     /// - Parameter initializer: The closure that will be called the very first time a value is needed
     public init(initializer: @escaping Initializer<Value>) {
-        x = initializer
-        x = {
+        valueHolder = initializer
+        valueHolder = {
             self.semaphore?.wait()
             
-            let initialValue = self.x()
-            self.x = { initialValue }
+            let initialValue = self.valueHolder()
+            self.valueHolder = { initialValue }
             
             self.semaphore?.signal()
             self.semaphore = nil
@@ -216,6 +220,6 @@ public class FunctionalLazy<_Value>: LazyPattern {
     /// If there is none, it is created using the initializer given when this struct was initialized. This process only
     /// happens on the first call to `value`; subsequent calls return the cached value from the first call.
     public var value: Value {
-        return x()
+        return valueHolder()
     }
 }
